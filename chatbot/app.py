665042ef7c3b9c8636b1038ccbb3e014e8589e28
Sanjeven <sanjeven@gmail.com>
2023-09-25 16:50:25 +0800

    updated UI

--- chatbot/app.py
+++ chatbot/app.py
@@ -20,6 +20,17 @@ app = Flask(__name__)
 def index():
     return render_template('chat.html')
 
+@app.route("/home")
+def home():
+    return render_template('chat.html')
+
+@app.route("/contact")
+def contact():
+    return render_template('contact.html')
+
+@app.route("/about")
+def about():
+    return render_template('about.html')
 
 @app.route("/get", methods=["GET", "POST"])
 def chat():
