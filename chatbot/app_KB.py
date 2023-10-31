03c0020f99d74361b84d4ff2a0a2258743708eb9
Sanjeven <sanjeven@gmail.com>
2023-10-31 16:02:38 +0800

    -

--- chatbot/app_KB.py
+++ chatbot/app_KB.py
@@ -69,36 +69,14 @@ def chat():
 
 def get_Chat_response(text):
     global last_reply
-    #nlp = spacy.load("en_core_web_sm")
-    # Create a new chat bot named Charlie
     chatbot = ChatBot('Charlie')
 
     trainer = ListTrainer(chatbot)
 
     threshold=0
-    #question = "fish"
-    #question = "what does Conversational AI refer to?"
 
     response = get_answer(covid_faq_chatbot, text, threshold)
-    #trainer = ChatterBotCorpusTrainer(chatbot)
-    
-    #trainer.train('C:\\Users\\sanje\\OneDrive\\Documents\\GIT\\GCPLP_practice_module\\GCPLP_practice_module\\chatbot\\covid.yml')
-    
-    """
-    # Load data from CSV
-    df = pd.read_csv('testforkb.csv')
 
-    # Convert the DataFrame to a list of dictionaries
-    custom_data = df.to_dict(orient='records')
-
-    trainer = ListTrainer(chatbot)
-
-    # Train the chatbot using the custom data
-    trainer.train(custom_data)
-    
-    response = str(chatbot.get_response(text))
-    print (response)
-    """
     last_reply = response
     last_reply = last_reply.replace("<b>", "*")
     last_reply = last_reply.replace("</b>", "*")
@@ -125,32 +103,21 @@ def faq_chatbot_initialize(chatbot_name, threshold=0.9, excel_path= 'C:\\Users\\
         read_only=True,
     )
     trainer = ListTrainer(covid_faq_chatbot)
-    #trainer.train("chatterbot.corpus.english")
-    # read questions and answers
+
     data = pd.read_excel(excel_path, sheet_name=worksheet_name, engine='openpyxl')
     question = data.get('Ingredients')
     answer = data.get('Recipe')
 
-    #for i in range(0, 3):
-    #    print('[Q]', question[i], '\n[A]', answer[i], '\n\n')
-
-    # Iteratively adding the question and answer
     train_list = []
     for i in range(len(question)):
         train_list.append(question[i])
         train_list.append(answer[i])
-    # train the faq
     trainer.train(train_list)
-    #trainer.export_for_training('covid.yml')
-    print("was here")
     return covid_faq_chatbot
 
 def get_answer(faq_chatbot, question, threshold):  # let's get a response to our input
-    # try suggested corpora to find best fit. If first corpus < theshold, try another.
-    # avoid random responses confidence 0
     response = faq_chatbot.get_response(question)
     if  response.confidence < threshold:  # not a good answer, look elsewhere
-        #response = nlp_chatbot.get_response(question)
         print(response)
         print(response.confidence)
         response = 'We do not have such a combination in our corpus. Remove some ingredients and try again.'
@@ -161,12 +128,9 @@ def get_answer(faq_chatbot, question, threshold):  # let's get a response to our
         response = response.replace("splt","<br>")
         print (response)
     return response
-#import logginglogging.basicConfig(level=logging.INFO)    # Enable info level logging
-
     
 if __name__ == '__main__':
-    data_path = 'data/'
-    excel_name =  'C:\\Users\\sanje\\data\\testforkb.xlsx'
+    excel_name =  'data/kb_data.xlsx'
     worksheet_name = 'Recipe'
 
     print ("train")
